f579ad7a700257a413c1ab281f9fe73c8db5db1a
Josebaseba <josebalega@gmail.com>
2014-09-02 15:45:20 +0200

    Added https support

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = function(sails) {
 
         '/*': function redirectToNonWWW(req, res, next) {
           if(req.headers && req.headers.host && req.headers.host.match(/^www/) !== null){
-            return res.redirect(301, 'http://' + req.headers.host.replace(/^www\./, '') + req.url);
+            return res.redirect(301, req.protocol + '://' + req.headers.host.replace(/^www\./, '') + req.url);
           }
           return next();
         }
